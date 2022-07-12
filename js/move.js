ee9d512be9f19f65f10c62f45aa6930c1d7bc291
zzangyu <hellohan95@gmail.com>
2022-07-12 23:57:07 +0900

    frame

---
+++ js/move.js
@@ -0,0 +1,12 @@
+function moveFrance() {
+	location.href = "france.html";
+}
+function moveSwis() {
+	location.href = "switzerland.html";
+}
+function moveJapan() {
+	location.href = "Japan.html";
+}
+function moveItaly() {
+	location.href = "italy.html";
+}
