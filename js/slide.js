da3ca1254e36cb3ed3c8640b3aee9a8b29a012ed
zzangyu <hellohan95@gmail.com>
2022-07-13 20:18:45 +0900

    home

---
+++ js/slide.js
@@ -0,0 +1,16 @@
+
+let slideIndex = 0;
+showSlide();
+function showSlide() {
+    let i;
+    let x = document.getElementsByClassName("slide");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > x.length) {
+        slideIndex = 1;
+    }
+    x[slideIndex - 1].style.display = "block";
+    setTimeout(showSlide, 2000);
+}
